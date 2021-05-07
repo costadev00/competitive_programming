884cb712007470c62585438a143b88ab9af53964
Matheus Costa Monteiro <43221564+costadev00@users.noreply.github.com>
2021-05-07 20:37:20 -0300

    find updated

--- STL/findSTL.cpp
+++ STL/findSTL.cpp
@@ -14,9 +14,11 @@ int main()
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
     vector<int> vect(arr, arr + n);
-    if (find(vect.begin(), vect.end(), 3) != vect.end())
+    vector<int>::iterator it;
+    it = find(vect.begin(), vect.end(), 3);
+    if (it != vect.end())
     {
-        cout << "Founded!\n";
+        cout << "Founded at position: " << it - vect.begin() << endl;
     }
     else
     {
